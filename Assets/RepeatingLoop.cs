88adf4029d735fa7fa1a4ce8ee09f0cc58f2c7b5
Walexe <64965115+Walexe@users.noreply.github.com>
2020-05-07 15:28:13 +0200

    Add files via upload

---
+++ Assets/RepeatingLoop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingLoop : MonoBehaviour
+{
+    private BoxCollider2D groundCollider;
+    private float groundHorizontalLenght;
+
+	// Use this for initialization
+	void Start ()
+    {
+        groundCollider = GetComponent<BoxCollider2D>();
+        groundHorizontalLenght = groundCollider.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (transform.position.x < - groundHorizontalLenght)
+        {
+            RepositionBackground ();
+        }
+	}
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2(groundHorizontalLenght * 2f, 0);
+        transform.position = (Vector2)transform.position + groundOffset;
+    }
+}
---
+++ Assets/gameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameController : MonoBehaviour
+{
+    public static gameController instance;
+    public GameObject gameovertext;
+    public bool gameover = false;
+    public Text scoreText;
+    public float scrollspeed = -1.5f;
+
+    private int score = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (gameover == true && Input.GetMouseButtonDown (0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+    
+    public void BirdScored ()
+    {
+        if (gameover)
+        {
+            return; 
+        }
+        score++;
+        scoreText.text = "Score" + score.ToString ();
+    }
+
+
+    public void Birddied ()
+    {
+        gameovertext.SetActive (true);
+        gameover = true;
+    }
+}
